c9d0cdd2eb9b6ef9559a2aaf548bd749280554f3
Divy Srivastava <dj.srivastava23@gmail.com>
2025-02-12 11:33:08 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+encoded string with addresses and stuff. API symbolizes the addresses. addr2line-pdb
+
+https://github.com/oven-sh/bun.report/
---
+++ src/error.rs
@@ -0,0 +1,29 @@
+use axum::{http, response::IntoResponse, Json};
+use serde::Serialize;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub(crate) enum ApiError {
+  #[error("Reached maximum number of referrals")]
+  InvalidFrameString,
+
+  #[error("PDB error: {0}")]
+  PdbError(#[from] pdb_addr2line::Error),
+}
+
+impl IntoResponse for ApiError {
+  fn into_response(self) -> axum::response::Response {
+    #[derive(Serialize)]
+    struct ErrResponse {
+      message: String,
+    }
+
+    let status = match self {
+      Self::InvalidFrameString => http::StatusCode::BAD_REQUEST,
+      Self::PdbError(_) => http::StatusCode::INTERNAL_SERVER_ERROR,
+    };
+
+    let message = format!("{}", self);
+    (status, Json(ErrResponse { message })).into_response()
+  }
+}
