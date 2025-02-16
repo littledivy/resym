ffefedb3f8b29ad6d71dea72c836c4fd4ef33b8f
Divy Srivastava <dj.srivastava23@gmail.com>
2025-02-16 10:17:34 +0530

    Fix

--- src/vlq.rs
+++ src/vlq.rs
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-use std::io::{self, Error, ErrorKind};
+use std::io::{Error, ErrorKind};
 
 fn base64_lut(byte: u8) -> u8 {
   match byte {
